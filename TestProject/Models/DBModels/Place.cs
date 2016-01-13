4df048a559eebf1a0a687857c4110c561f012456
Suharsha123 <suharshafonseka44@gmail.com>
2016-01-13 15:58:42 +0530

    message

--- TestProject/Models/DBModels/Place.cs
+++ TestProject/Models/DBModels/Place.cs
@@ -13,23 +13,12 @@ namespace TestProject.Models.DBModels
         [Key]
         public Guid Id { get; set; }
 
-        [Column("Name", TypeName = "varchar")]
-        [StringLength(25)]
         public string PlaceName { get; set; }
-
-        [Column("City", TypeName = "varchar")]
-        [StringLength(25)]
         public string PlaceLocatedCity { get; set; }
-
-        [Column("Address", TypeName = "varchar")]
-        [StringLength(250)]
         public string Address { get; set; }
         public DateTime PlaceOpenTime { get; set; }
         public DateTime PlaceCloseTime { get; set; }
-
-        [Column(TypeName = "ntext")]
         public string Description { get; set; }
-
         [ForeignKey("District")]
         public Guid DistrictId { get; set; }
         public virtual District District { get; set; }
--- TestProject/Models/DBModels/PlaceImage.cs
+++ TestProject/Models/DBModels/PlaceImage.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,15 +9,9 @@ namespace TestProject.Models.DBModels
 {
     public class PlaceImage
     {
-
         [Key]
         public Guid Id { get; set; }
-
-        [ForeignKey("Place")]
-        public Guid PlaceId { get; set; }
-
-        public virtual Place Place { get; set; }
+        public virtual ICollection<Place> Event { get; set; }
         public bool Approved { get; set; }
-
     }
 }
