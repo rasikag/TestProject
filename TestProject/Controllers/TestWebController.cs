709661a05906061f4062b51cdcec0b7124cc53aa
Rasika Gayan <rasikagayangunarathna@gmail.com>
2016-01-19 02:21:43 +0530

    added angular image upload

---
+++ TestProject/Controllers/TestWebController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestProject.Controllers
+{
+    public class TestWebController : Controller
+    {
+        // GET: TestWeb
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
